6381f39a1b10e292ac397eafddbf0274813f92d9
KaioZaniboni <kaio.zaniboni@bandtec.com.br>
2021-05-23 17:04:05 -0300

    concluido 50%

--- NósTreidamus/Pós API/api-projeto-site/API-projeto-site/config/config.js
+++ NósTreidamus/Pós API/api-projeto-site/API-projeto-site/config/config.js
@@ -25,9 +25,9 @@ module.exports = {
   // Insira aqui seus dados do banco LOCAL - MySQL Workbench
   dev: {
     // altere APENAS username, password e database.
-    username: 'SEU USUARIO DO WORKBENCH, provavelmente é root',
-    password: 'SUA SENHA DO WORKBENCH',
-    database: 'SEU DATABASE DO CREATE DATABASE',
+    username: 'root',
+    password: 'R@phael1',
+    database: 'teste',
     host: '127.0.0.1',
     dialect: 'mysql',
     xuse_env_variable: 'DATABASE_URL',
